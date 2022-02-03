76b2b0dc8785160c42d9459c68f07c195f42b0de
didya <didyalife@gmail.com>
2022-02-03 15:24:10 +0200

    add check for current url after logout

--- src/test/java/pages/HeaderMenu.java
+++ src/test/java/pages/HeaderMenu.java
@@ -1,5 +1,6 @@
 package pages;
 
+import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -9,5 +10,6 @@ public class HeaderMenu {
     public void logOut(){
         $("app-user-nav").click();
         $(By.xpath("//button[text()='Logout']")).click();
+        WebDriverRunner.url().contains("https://qauto.forstudy.space/");
     }
 }
--- src/test/java/pages/SideBarMenu.java
+++ src/test/java/pages/SideBarMenu.java
@@ -1,11 +1,14 @@
 package pages;
 
 
+import com.codeborne.selenide.WebDriverRunner;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class SideBarMenu {
 
     public void logOut(){
         $(".text-danger").click();
+        WebDriverRunner.url().contains("https://qauto.forstudy.space/");
     }
 }
